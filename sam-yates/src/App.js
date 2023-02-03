01f683cf14e3955fca898705395b2852663e350b
hilaryyates <hilryyates@gmail.com>
2023-02-03 10:22:17 -0500

    colors

--- sam-yates/src/App.js
+++ sam-yates/src/App.js
@@ -1,12 +1,12 @@
+import React from "react";
 import "./App.css";
 import Nav from "./Nav/Nav";
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='Job-Title'>
-        <HTMLHeadingElement></HTMLHeadingElement>SY Consulting
-      </h1>
+      <h1 className='Job-Title'>SY Consulting</h1>
+      <img src='https://www.pngitem.com/pimgs/m/556-5567996_accounting-blue-financial-accounting-logo-png-transparent-png.png' />
       <Nav />
     </div>
   );
