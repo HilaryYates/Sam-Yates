07c48f4bd22b5d1df7ad99faf86904bbf71567ca
hilaryyates <hilryyates@gmail.com>
2023-02-03 10:36:49 -0500

    aligned text

--- sam-yates/src/App.js
+++ sam-yates/src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className='App'>
       <h1 className='Job-Title'>SY Consulting</h1>
-      <img src='https://www.pngitem.com/pimgs/m/556-5567996_accounting-blue-financial-accounting-logo-png-transparent-png.png' />
+      <img
+        className='Logo'
+        src='https://www.pngitem.com/pimgs/m/556-5567996_accounting-blue-financial-accounting-logo-png-transparent-png.png'
+      />
       <Nav />
     </div>
   );
